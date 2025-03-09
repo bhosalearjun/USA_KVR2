af04302f870ad9bf5aa3131d64f4226ea5e1ef82
bhosalearjun <bhosalearjun2459@gmail.com>
2025-03-09 15:38:21 +0530

    55 th code added

---
+++ practice_55.py
@@ -0,0 +1,23 @@
+def get_line():
+    return input("please enter the line:")
+
+def reverse_the_line(ghar_line):
+    words=ghar_line.split()
+    print(words)
+    print("-"*50)
+    print(' '.join(words[::-1]))
+
+
+
+
+
+
+
+
+
+
+
+
+line=get_line()
+
+reverse_the_line(line)
